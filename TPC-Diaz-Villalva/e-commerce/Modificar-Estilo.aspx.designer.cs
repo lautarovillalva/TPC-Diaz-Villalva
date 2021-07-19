20417e85106876028b6704bdccf184efcd7e2552
Lautaro Villalva <ljvillalva@gmail.com>
2021-07-19 18:21:05 -0300

    columna visible agregada en articulos, categorias, estilos, colores

--- TPC-Diaz-Villalva/e-commerce/Modificar-Estilo.aspx.designer.cs
+++ TPC-Diaz-Villalva/e-commerce/Modificar-Estilo.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace e_commerce
         protected global::System.Web.UI.WebControls.TextBox tbx_nombre;
 
         /// <summary>
+        /// Control cbx_visible.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox cbx_visible;
+
+        /// <summary>
         /// Control btnModificar.
         /// </summary>
         /// <remarks>
